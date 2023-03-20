153527aa4f07a689b81ea8363b6975f574477096
Seventty <elmastigeron2015@gmail.com>
2023-03-20 01:32:31 -0400

    Map array handler completed

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,38 +1,32 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import * as L from 'leaflet';
 import { MarkerService } from '../services/marker.service';
 import 'leaflet-draw';
-
-const iconRetinaUrl = 'assets/marker-icon-2x.png';
-const iconUrl = 'assets/marker-icon.png';
-const shadowUrl = 'assets/marker-shadow.png';
-const iconDefault = L.icon({
-  iconRetinaUrl,
-  iconUrl,
-  shadowUrl,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  tooltipAnchor: [16, -28],
-  shadowSize: [41, 41]
-});
-L.Marker.prototype.options.icon = iconDefault;
+import { Figuras } from '../enums/figuras.enum';
+import { MapOptions } from '../enums/mapOptions.enum';
 
 @Component({
-  selector: 'leaflet',
+  selector: 'LeafletMap',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements AfterViewInit {
+export class MapComponent implements OnInit {
+  @Input() ViewMode: boolean = false;
+  @Input() EditMode: boolean = false;
+  @Input() FormulacionId: number = 0;
+  @Input() TablaId: number = 0;
+  @Output() mapDataEmitter = new EventEmitter;
+
   public lat: number = 18.477923;
   public long: number = -69.933491;
   private map: any;
 
   public errorColor: string = "#f35858";
   public primaryColor: string = "#f37f58";
+  public figureType: Array<string> = ["polyline", "polygon", "rectangle", "circle", "marker", "circlemarker"]
+  public mapDataCollection: Array<any> = [];
 
   private initMap(): void {
-
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
       minZoom: 3,
@@ -71,52 +65,112 @@ export class MapComponent implements AfterViewInit {
             color: this.primaryColor
           }
         },
-        circle: true,
         rectangle: {
+          showArea: false,
           shapeOptions: {
-            clickable: true,
+            color: this.primaryColor
           }
         },
-
+        circle: {
+          shapeOptions: {
+            color: this.primaryColor
+          }
+        },
       },
       edit: {
         featureGroup: drawFeatures, //REQUIRED!!
-        remove: false
+        remove: true
       }
     };
 
     const drawControl = new L.Control.Draw(options);
-    this.map.addControl(drawControl);
+    if (!this.ViewMode) this.map.addControl(drawControl);
 
+    //! Creando figura en mapa
     this.map.on("draw:created", (e: any) => {
-      const type = e.layerType;
-      const layer = e.layer;
-
-      console.log("🚀 ~ file: map.component.ts:102 ~ MapComponent ~ this.map.on ~ e:", layer)
+      const { layer, layerType } = e;
+      const option = MapOptions.Created;
       drawFeatures.addLayer(layer);
+      this.drawHandler(layer, layerType, option);
     });
 
-    this.map.on("draw:edited", function(e: any){
-      const type = e.layerType;
-      const layers = e.layer;
+    //! Editando figura en mapa
+    this.map.on("draw:edited", (e: any) => {
+      console.log("EVENTO EDITAR", e);
+      const option = MapOptions.Edited;
 
-      layers.eachLayer(function(e: any){
-        console.log(e)
+      e.layers.eachLayer((layer: any) => {
+        this.drawHandler(layer, layer.toGeoJSON().geometry.type.toLowerCase(), option)
       })
-    })
+    });
+
+    //! Borrando figura en mapa
+    this.map.on("draw:deleted", (e: any) => {
+      const { layers, layerType } = e;
+      const option = MapOptions.Deleted;
+
+      e.layers.eachLayer((layer: any) => {
+        this.deleteDataFromCollection(layer._leaflet_id);
+      })
+    });
 
     tiles.addTo(this.map);
   }
 
-  constructor(private markerService: MarkerService) { }
-  ngAfterViewInit(): void {
-    this.localDrawConfigurator()
-    this.initMap();
-    //this.markerService.makeCapitalMarkers(this.map);
+  drawHandler(drawEventLayer: any, drawEventLayerType: any, option: string) {
+    const { coordinates } = drawEventLayer.toGeoJSON().geometry;
+    const figuraId = drawEventLayer._leaflet_id;
+    const tipoGeometriaId = this.figureType.indexOf(drawEventLayerType);
+
+    switch (option) {
+      case "created":
+        this.pushDataToCollection(this.mappedData(figuraId, tipoGeometriaId, coordinates));
+        // TODO: Mapear todos los objetos & hacerle push al arreglo
+        break;
+      case "edited":
+        this.editDataInCollection(figuraId, this.mappedData(figuraId, tipoGeometriaId, coordinates))
+        // TODO: Identificar el id del objeto pusheado para sobreescribirlo
+        break;
+      case "deleted":
+        //this.deleteDataFromCollection(figuraId)
+        // TODO: Identificar el id del objeto pusheado para borrarlo de la colección del arreglo
+        break;
+      default:
+        break;
+    }
   }
 
+  mappedData(figuraId: number, tipoGeometria: number, coordenadas: Array<any>) {
+    return {
+      id: this.TablaId,
+      formulacionId: this.FormulacionId,
+      figuraId: figuraId,
+      tipoGeometriaId: tipoGeometria,
+      coordenadas: coordenadas
+    }
+  }
 
-  localDrawConfigurator(){
+  pushDataToCollection(data: any){
+    this.mapDataCollection.push(data);
+    this.mapDataEmitter.emit(this.mapDataCollection);
+  }
+
+  editDataInCollection(figuraId: number, newDraw: any){
+    const index = this.mapDataCollection.findIndex(drawObject => drawObject.figuraId === figuraId);
+    this.mapDataCollection[index] = newDraw;
+    this.mapDataEmitter.emit(this.mapDataCollection);
+  }
+
+  deleteDataFromCollection(figuraId: number){
+    const index = this.mapDataCollection.findIndex(drawObject => drawObject.figuraId === figuraId);
+    if (index !== -1) {
+      this.mapDataCollection.splice(index, 1);
+    }
+
+    this.mapDataEmitter.emit(this.mapDataCollection);
+  }
+
+  localDrawConfigurator() {
     L.drawLocal.draw.toolbar.buttons.polyline = 'Herramienta para dibujar una Linea';
     L.drawLocal.draw.toolbar.buttons.polygon = 'Herramienta para dibujar un Poligono';
     L.drawLocal.draw.toolbar.buttons.rectangle = 'Herramienta para dibujar un Rectangulo';
@@ -130,5 +184,17 @@ export class MapComponent implements AfterViewInit {
     L.drawLocal.draw.handlers.circle.tooltip.start = 'Haz click y arrastra para crear un radio de Circulo';
     L.drawLocal.draw.handlers.marker.tooltip.start = 'Haz click sobre cualquier lugar del mapa para poner un Punto de Referencia';
     L.drawLocal.draw.handlers.circlemarker.tooltip.start = 'Haz click y arrastra para crear un marcador Circular';
+    L.drawLocal.draw.toolbar.finish.text = "Finalizar";
+    L.drawLocal.draw.toolbar.finish.title = "Finaliza el trazado";
+    L.drawLocal.draw.toolbar.undo.text = "Revertir punto trazado";
+    L.drawLocal.draw.toolbar.undo.title = "Revertir el ultimo punto trazado punto";
+  }
+
+  constructor(private markerService: MarkerService) { }
+
+  ngOnInit(): void {
+    this.localDrawConfigurator()
+    this.initMap();
+    //this.markerService.makeCapitalMarkers(this.map);
   }
 }
